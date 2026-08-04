87638ebe1f580621d0ec34fb409de379f2844c07
wovidio25-star <wovidio25@gmail.com>
2026-08-03 22:54:49 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'finanzas-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Instalar el Service Worker y guardar los archivos en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Responder a las solicitudes (cargar desde caché si no hay internet)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
